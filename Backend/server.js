572d15191f4c2fd8619de20063100c971deaa657
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-04-26 00:17:47 +0530

    hey

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,9 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRouter from './routes/userRoute.js'
+import adminRouter from './routes/adminRouter.js'
+import doctorRouter from './routes/doctorRouter.js'
+import userRouter from './routes/userRouter.js'
 
 // app config
 const app = express()
