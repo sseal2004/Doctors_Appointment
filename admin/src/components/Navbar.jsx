b685bfa851a61a505459db8c66ee4bbf34d2e918
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-05-04 14:07:29 +0530

    done ready for hosting

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   }
 
   const goToUserPanel = () => {
-    window.location.href = 'http://localhost:5175/'
+    window.location.href = 'https://doctors-appointment-ashy.vercel.app/'
   }
 
   const isOnDashboard =
