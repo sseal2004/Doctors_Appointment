f5dbb9d3dd5200abbdd6d4d221cfd896ab4c76c7
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-05-04 14:35:13 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import mongoose from 'mongoose'
+
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+
 import adminRouter from './routes/adminRouter.js'
 import doctorRouter from './routes/doctorRouter.js'
 import userRouter from './routes/userRouter.js'
 
-// app config
 const app = express()
-const port = process.env.PORT || 4000
 
-// Connect to database (CALL THE FUNCTION)
 connectDB()
 connectCloudinary()
 
@@ -19,25 +19,21 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
-// api endpoints
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
-app.use("/api/user", userRouter)
-
+app.use('/api/user', userRouter)
 
 app.get("/", (req, res) => {
-  res.send("API Working")
-});
+  res.send("API Working ")
+})
 
 app.get('/test-db', (req, res) => {
-  const state = mongoose.connection.readyState;
-  // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+  const state = mongoose.connection.readyState
   if (state === 1) {
-    res.send('Database is connected');
+    res.send('Database is connected ')
   } else {
-    res.status(500).send('Database is NOT connected');
+    res.status(500).send('Database is NOT connected ❌')
   }
-});
+})
 
-
-app.listen(port, () => console.log(`Server started on PORT:${port}`))
+export default app
